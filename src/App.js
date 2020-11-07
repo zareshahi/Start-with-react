55d2eb4bfe8d06e5a247cfc81ef3f86842cee7df
Ali Zareshahi <a.zareshahi1377@gmail.com>
2020-11-07 08:11:42 +0330

    feat(Components): initial navbar and import all components to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import NavBar from './components/navBar';
+import Counters from './components/counters';
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className='container'>
+          <Counters />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Counters from './components/counters';
 import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
