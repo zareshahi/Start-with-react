40d2a0f215c912ad5fc3545c3401006468969b63
Ali Zareshahi <a.zareshahi1377@gmail.com>
2020-11-07 07:57:36 +0330

    style(Main): Eslint auto reformat index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Counters from './components/counters';
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
